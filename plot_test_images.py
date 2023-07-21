5cb8699eaa8de3a26181c5ee328e1a004ca81c45
unknown <hyukahn@umich.edu>
2023-07-21 14:09:26 +0900

    Clean up comments

--- plot_test_images.py
+++ plot_test_images.py
@@ -7,7 +7,6 @@ from PIL import Image
 import numpy as np
 from torchvision import transforms
 from torchvision.ops import box_iou
-# from avg_precision import get_AP
 
 device = "cuda:0"
 model = YOLOv3Darknet().eval().to(device)
@@ -23,35 +22,32 @@ for f in os.listdir(dir):
   if not os.path.isfile(img_path):
     continue
     
-  # TODO: Load image file as tensor
+  # Load image file as tensor
   test_img = Image.open(img_path).convert('RGB')
   convert_tensor = transforms.ToTensor()
   test_img = convert_tensor(test_img).to(device)
-  test_img = test_img[None, :]
-  print(test_img.shape)
+  test_img = test_img[None, :] # Turn dim to batch dim
+
   # Get the labels for the testing images
   output = model(test_img)
   
   person_label = 0
   output = utils.get_region_boxes_general(output, model, conf_thresh=conf_thresh, name=architecture)
   for i, boxes in enumerate(output):
+    # Create and open label file
     label_file_name = f.split(".")[0] + ".txt"
     label_file_dir = os.path.join(labels_dir, label_file_name)
     label_f = open(label_file_dir, 'w') # Create and open file
 
+    # Skip this image if no boxes were predicted
     if len(boxes) == 0:
         label_f.close()
         continue
     assert boxes.shape[1] == 7
   
+    # Get bounding box information
     test_nms_thresh = 1.0 # FIXME:??? 1.0???
     boxes = utils.nms(boxes, nms_thresh=test_nms_thresh)
-    # w1 = boxes[..., 0] - boxes[..., 2] / 2
-    # h1 = boxes[..., 1] - boxes[..., 3] / 2
-    # w2 = boxes[..., 0] + boxes[..., 2] / 2
-    # h2 = boxes[..., 1] + boxes[..., 3] / 2
-    # bboxes = torch.stack([w1, h1, w2, h2], dim=-1)
-    # bboxes = bboxes.view(-1, 4).detach() * self.img_size
     x_centers = boxes[..., 0]
     y_centers = boxes[..., 1]
     widths = boxes[..., 2]
@@ -59,31 +55,30 @@ for f in os.listdir(dir):
     det_confs = boxes[..., 4]
     labels = boxes[..., 6]
 
-    # FIXME: there can be multiple boxes
+    # Save box info to label file
     for i in range(len(boxes)):
       if labels[i] == person_label:
-        # Save box info to label file
         label_f.write(f'{labels[i]} {x_centers[i]} {y_centers[i]} {widths[i]} {heights[i]} {det_confs[i]}\n') 
     label_f.close()
 
-# # 2. PLOT LABELS #
-# plot_dir = os.path.join(dir, "plotted") # where to save plotted images
+# 2. PLOT LABELS
+plot_dir = os.path.join(dir, "plotted") # where to save plotted images
 
-# # Find all patched image names
-# patched_imgs = [f for f in os.listdir(dir) 
-#              if os.path.isfile(os.path.join(dir, f))]
+# Find all patched image names
+patched_imgs = [f for f in os.listdir(dir) 
+             if os.path.isfile(os.path.join(dir, f))]
 
-# # Make sure plot saving folder exists
-# if not os.path.exists(plot_dir):
-#     os.mkdir(plot_dir)
+# Make sure plot saving folder exists
+if not os.path.exists(plot_dir):
+    os.mkdir(plot_dir)
 
-# # Draw boxes
-# for img_name in patched_imgs:
-#     img_dir = os.path.join(dir, img_name)
-#     label_dir = os.path.join(labels_dir, img_name.split(".")[0] + ".txt")
-#     img = Image.open(img_dir).convert('RGB')
-#     with open(label_dir) as label:
-#         boxes = label.readlines()
-#         boxes = np.array([b.strip('\n').split() for b in boxes], dtype=float)
-#         boxes = [[b[0], b[2], b[1], b[4], b[3]] for b in boxes]
-#         plot_boxes(img, boxes, savename=os.path.join(plot_dir, img_name))
+# Draw boxes
+for img_name in patched_imgs:
+    img_dir = os.path.join(dir, img_name)
+    label_dir = os.path.join(labels_dir, img_name.split(".")[0] + ".txt")
+    img = Image.open(img_dir).convert('RGB')
+    with open(label_dir) as label:
+        boxes = label.readlines()
+        boxes = np.array([b.strip('\n').split() for b in boxes], dtype=float)
+        boxes = [[b[0], b[2], b[1], b[4], b[3]] for b in boxes]
+        plot_boxes(img, boxes, savename=os.path.join(plot_dir, img_name))
