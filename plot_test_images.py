ff120fbc50bfe930a897ce4f42dfab2cdafd959f
unknown <hyukahn@umich.edu>
2023-07-25 10:06:38 +0900

    Create missing files

--- plot_test_images.py
+++ plot_test_images.py
@@ -8,6 +8,7 @@ import numpy as np
 from torchvision import transforms
 from torchvision.ops import box_iou
 
+# 1. GET AND SAVE LABELS FROM YOLO
 device = "cuda:0"
 model = YOLOv3Darknet().eval().to(device)
 model.load_darknet_weights('arch/weights/yolov3.weights')
@@ -16,9 +17,16 @@ conf_thresh = 0.5 # FIXME
 architecture = "yolov3"
 
 dir = "./test_images" # where the testing images are
+if not os.path.exists(dir):
+  print("Created ./test_images directory")
+  os.makedirs(dir)
 labels_dir = os.path.join(dir, "yolo-labels") # where the testing images labels are
-for f in os.listdir(dir):
-  img_path = os.path.join(dir, f)
+if not os.path.exists(labels_dir):
+   os.makedirs(labels_dir)
+for img_f in os.listdir(dir):
+  img_path = os.path.join(dir, img_f)
+
+  # Check if image file or not
   if not os.path.isfile(img_path):
     continue
     
@@ -35,7 +43,7 @@ for f in os.listdir(dir):
   output = utils.get_region_boxes_general(output, model, conf_thresh=conf_thresh, name=architecture)
   for i, boxes in enumerate(output):
     # Create and open label file
-    label_file_name = f.split(".")[0] + ".txt"
+    label_file_name = img_f.split(".")[0] + ".txt"
     label_file_dir = os.path.join(labels_dir, label_file_name)
     label_f = open(label_file_dir, 'w') # Create and open file
 
@@ -61,6 +69,9 @@ for f in os.listdir(dir):
         label_f.write(f'{labels[i]} {x_centers[i]} {y_centers[i]} {widths[i]} {heights[i]} {det_confs[i]}\n') 
     label_f.close()
 
+###############################################################################
+###############################################################################
+###############################################################################
 # 2. PLOT LABELS
 plot_dir = os.path.join(dir, "plotted") # where to save plotted images
 
