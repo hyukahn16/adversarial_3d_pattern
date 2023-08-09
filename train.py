cc5ebf3d2c0d3af52e536874e7fd6d9a170167d1
unknown <hyukahn@umich.edu>
2023-08-09 14:50:07 +0900

    Remove iterating over labels from train loader

--- train.py
+++ train.py
@@ -487,7 +487,7 @@ class PatchTrainer(object):
                 tau = np.exp(-(epoch + 1) / args.nepoch * args.anneal_alpha) * args.anneal_init
             else:
                 tau = 0.3
-            for i_batch, (img_batch, lab_batch) in tqdm(enumerate(self.train_loader), desc=f'Epoch {epoch}'):
+            for i_batch, img_batch in tqdm(enumerate(self.train_loader), desc=f'Epoch {epoch}'):
             # for i_batch, img_batch in enumerate(self.train_loader):
                 img_batch = img_batch.to(self.device)
                 # t0 = time.time()
